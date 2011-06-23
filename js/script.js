61f96ae396bc92eff79c5522c0f806d06d71aec4
Jacob Fierro <jacob.fierro@gmail.com>
2011-06-23 17:58:35 -0400

    work nav is in

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$(function(){
+var jacobfierro = {};
 
-});
+
+(function(context, undefined){
+	
+	function getCurrentPageName() {
+		var full = $(location).attr('href');
+		return full.substring(full.lastIndexOf('/')+1, full.lastIndexOf('.'));
+	}
+	
+	context.highlightNav = function() {
+		var current = getCurrentPageName();
+		$('#header').find('.'+current).addClass('active');
+	}
+	
+	
+})(jacobfierro);
+
+
+$(function(){
+	jacobfierro.highlightNav();
+});
