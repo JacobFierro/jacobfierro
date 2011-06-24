59e0b3ffec3c0fcf125e89c90a31bc7cc70b26c1
Jacob Fierro <jacob.fierro@gmail.com>
2011-06-23 21:59:12 -0400

    work bg-img switching. work active states. beginning to put in work content

--- js/script.js
+++ js/script.js
@@ -3,20 +3,72 @@ var jacobfierro = {};
 
 (function(context, undefined){
 	
-	function getCurrentPageName() {
+	function get_hash() {
+		var href = $(location).attr('href');
+		var retval = "";
+		if (href.search('#') === -1) {
+			retval = get_first_work_id();
+		} else {
+			var hash = href.substring(href.lastIndexOf('#')+1);
+			if (hash.length === 0) {
+				retval = get_first_work_id();
+			} else {
+				retval = hash;
+			}
+		}
+		return retval;
+	}
+	
+	function get_first_work_id() {
+		return $('.workNav').find('a:first').attr('id');
+	}
+	
+	function set_background_image(path) {
+		$('body').css({ 'background-image' : 'url(' + path + ')' });
+	}
+	
+	function set_active_work(name) {
+		set_background_image('img/' + name + '.jpg');
+		$('.workNav').find('a').removeClass('active');
+		$('#'+name).addClass('active');
+		
+		$('.content').remove();
+		
+		$.get('work/'+name+'.html', function(data){	
+			$('.context').after(data);
+		});
+		
+	}
+	
+	/*
+	** PUBLIC
+	*/
+	
+	context.work_handler = function() {
+		set_active_work(get_hash());
+		
+		$('.workNav').find('a').click(function(e){
+			set_active_work( $(this).attr('id') );
+		});
+	}
+	
+	context.getCurrentPageName = function() {
 		var full = $(location).attr('href');
 		return full.substring(full.lastIndexOf('/')+1, full.lastIndexOf('.'));
 	}
 	
-	context.highlightNav = function() {
-		var current = getCurrentPageName();
+	context.highlight_main_nav = function() {
+		var current = this.getCurrentPageName();
 		$('#header').find('.'+current).addClass('active');
 	}
-	
 	
 })(jacobfierro);
 
 
 $(function(){
-	jacobfierro.highlightNav();
+	jacobfierro.highlight_main_nav();
+	if(jacobfierro.getCurrentPageName() === 'work') {
+		jacobfierro.work_handler();
+	}
+	
 });
