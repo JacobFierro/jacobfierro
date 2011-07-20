f6bb5bad7636a494f04d96a01745986d8a29a171
Jacob Fierro <jacob.fierro@gmail.com>
2011-07-20 15:28:20 -0400

    redid the js for nav highlighting, all is well

--- js/script.js
+++ js/script.js
@@ -11,87 +11,85 @@ var jf = {};
 		return vars;
 	}
 	
-	function get_first_work_id() {
-		return $('.secondary_nav').find('a:eq(1)').attr('id');
+	function get_first_work() {
+		return $('.secondary_nav').find('a:eq(1)').attr('class');
+	}
+	
+	function get_current_context() {
+		return $('.content').attr('id');
 	}
 	
 	function set_background_image(path) {
 		$.backstretch(path, {speed: 150});
 	}
 	
-	function set_active_work(name) {
-		set_background_image('img/' + name + '.jpg');
-		$('.secondary_nav').find('a').removeClass('active');
-		$('#'+name).addClass('active');
+	function set_active_work(work) {
+		set_background_image('img/' + work + '.jpg');
+		highlight_secondary_nav(work)
 		
 		$('.content').remove();
 		
-		$.get('work/'+name+'.html', function(data){	
+		$.get('work/'+work+'.html', function(data){	
 			$('.context').after( innerShiv(data, false) );
 			$('a.gallimg').fancybox();
 		});
-		
+	}
+	
+	function highlight_primary_nav(context) {
+		$('.primary_nav').find('.nav_'+context).addClass('active');
+	}
+	
+	function highlight_secondary_nav(active) {
+		$('.secondary_nav').find('a').removeClass('active');
+		$('.'+active).addClass('active');
+	}
+	
+	function form_success(response, status, xhr, $form) {
+		$('#response').hide().html(response).fadeIn();
+	    $('#contactform').slideUp();
 	}
 	
 	/*
 	** PUBLIC
 	*/
 	
-	context.work_init = function() {
-		var query = getUrlVars()['v'];
-		var work ="";
-		if (query === undefined) {
-			work = get_first_work_id();
-		} else {
-			work = (query.length > 0) ? query : get_first_work_id();
+	context.init = function() {
+		//determine the context we're in: home, work or contact
+		var context = get_current_context()
+		
+		highlight_primary_nav(context); //hl main nav
+		
+		//secondary nav and context specific functionality
+		if (context === "home") {
+			$('.home').addClass('active');
+			$.backstretch("img/home.jpg", {speed: 200});
+		} else if (context === "work") {
+			var query = getUrlVars()['v'];
+			var work ="";
+			if (query === undefined) {
+				work = get_first_work();
+			} else {
+				work = (query.length > 0) ? query : get_first_work();
+			}
+			set_active_work(work);
+		} else if (context === "contact") {
+			$("#contactform").validate({
+				submitHandler: function(form) {
+					$(form).ajaxSubmit({
+						success: form_success
+					});
+				},
+				invalidHandler: function(form, validator) {
+					log('invalid');
+				}
+			});
 		}
-		set_active_work(work);
-	}
-	
-	context.getCurrentPageName = function() {
-		var full = $(location).attr('href');
-		if (full.lastIndexOf('.') === -1) {
-			return full.substring( (full.lastIndexOf('/') +1) );
-		} else {
-			return full.substring( (full.lastIndexOf('/') +1), full.lastIndexOf('.') )
-		}
-	}
-	
-	context.highlight_nav = function(current) {
-		$('.primary_nav').find('.'+current).addClass('active');
-	}
-	
-	context.form_success = function(response, status, xhr, $form) {
-		$('#response').hide().html(response).fadeIn();
-	    $('#contactform').slideUp();
+		
 	}
 	
 })(jf);
 
 
 $(document).ready(function(){
-	var current = (jf.getCurrentPageName() === "") ? "index" : jf.getCurrentPageName();
-	
-	jf.highlight_nav(current);
-	
-	if (current === 'index') {
-		$('#home').addClass('active');
-		$.backstretch("img/home.jpg", {speed: 200});
-	} else if (jf.getCurrentPageName() === 'work') {
-		jf.work_init();
-	} else if (jf.getCurrentPageName() === 'contact') {
-	
-	
-	$("#contactform").validate({
-		submitHandler: function(form) {
-			$(form).ajaxSubmit({
-				success: jf.form_success
-			});
-		},
-		invalidHandler: function(form, validator) {
-			log('invalid');
-		}
-	});	
-}
-	
+	jf.init();
 });
